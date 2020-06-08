6aa2c20bd6eb2326698e64fa31d65ae1449e25cf
Nishit Basak <n.basak16@gmail.com>
2020-06-08 22:50:23 +0530

    Updates

---
+++ precache-manifest.8a235a805afaadf3158c6504d0ab2117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e49df6626e0587d8986f506a0c3b92e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "447564ac88991400de4c",
+    "url": "/monsters-rolodex/static/css/main.6bf5de0a.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monsters-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447564ac88991400de4c",
+    "url": "/monsters-rolodex/static/js/main.602bf2b2.chunk.js"
+  },
+  {
+    "revision": "bc9b3e30e5d8354d0e21",
+    "url": "/monsters-rolodex/static/js/runtime-main.bdaefb98.js"
+  }
+]);
